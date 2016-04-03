23cb657321a67c8370498d0343383ae438549869
Sidharth Shah <sidharth@fafadiatech.com>
2016-04-03 23:20:04 +0530

    added sample code from first example

---
+++ javascript/angular/samples/example1.js
@@ -0,0 +1,11 @@
+var app = angular.module('app', []);
+
+app.controller('helloworld', function($scope){
+	$scope.message = "Hello World!";
+	$scope.first = 1;
+	$scope.second = 999;
+
+	$scope.updateValue = function(){
+		$scope.result = $scope.first + $scope.second;
+	}
+});
