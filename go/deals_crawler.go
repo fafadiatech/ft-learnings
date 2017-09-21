86ea899a32fbe94a96f2febe0b9cac9f59be5983
Sidharth Shah <sidharth@fafadiatech.com>
2017-09-21 15:05:27 +0530

    added a simple deal crawler written using go routines

---
+++ go/deals_crawler.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Page struct {
+	url string
+}
+
+type Crawler struct {
+	frontier []Page
+}
+
+var done = make(chan bool)
+
+func NewCrawler(seedlist []Page) *Crawler {
+	c := Crawler{}
+	c.frontier = seedlist
+	return &c
+}
+
+func (c *Crawler) fetch_page(current_page *Page) {
+	msg := fmt.Sprintf("Fetching page:%s", current_page.url)
+	fmt.Println(msg)
+
+	resp, err := http.Get(current_page.url)
+
+	if err != nil {
+		fmt.Println("Error fetching")
+	}
+
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	fmt.Print("Fetched Bytes:")
+	fmt.Print(len(bytes))
+	fmt.Println("")
+
+	done <- true
+}
+
+func (c *Crawler) crawl() {
+	for _, page := range c.frontier {
+		go c.fetch_page(&page)
+	}
+
+}
+
+func main() {
+	deals_crawler := NewCrawler(
+		[]Page{
+			Page{"https://www.snapdeal.com/offers/deal-of-the-day"},
+			Page{"https://www.flipkart.com/"},
+			Page{"http://www.amazon.in/gp/goldbox/"},
+			Page{"http://www.jabong.com/deals/deal-of-the-day/"},
+			Page{"http://www.myntra.com/deal-of-the-day"},
+			Page{"https://www.nykaa.com/offers.html"},
+		},
+	)
+	deals_crawler.crawl()
+	for _, i := range deals_crawler.frontier {
+		<-done
+		msg := fmt.Sprintf("Done Crawling:%s", i)
+		fmt.Println(msg)
+	}
+}
