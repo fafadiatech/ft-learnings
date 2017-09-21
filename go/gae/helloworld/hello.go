b094c6e080da4c968dea22efe97c1f3c0f706d5c
Sidharth Shah <sidharth@fafadiatech.com>
2017-09-21 19:40:34 +0530

    added one more handler

--- go/gae/helloworld/hello.go
+++ go/gae/helloworld/hello.go
@@ -1,14 +1,19 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/quote", quote_handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+	fmt.Fprint(w, "Hello, world!")
+}
+
+func quote_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Lets do it together - Said no one ever!")
 }
