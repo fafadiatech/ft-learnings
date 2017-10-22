1c7497f67fa31b08389a98b1f46bfcd7da7e72fb
Sidharth Shah <sidharth@fafadiatech.com>
2017-10-22 11:52:10 +0530

    added couple of friendly comment

--- c/c_pointers.c
+++ c/c_pointers.c
@@ -2,11 +2,16 @@
 
 int main(){
   int val;
+  
+  // Pointer to an integer
   int *ptr;
   
   val = 10; 
+  
+  // Referencing using * operator
   ptr = &val;
-
+  
+  // Dereferencing using ** operator
   printf("Value {before change} from pointer is:%d\n", *ptr);
   *ptr = 20;
   printf("Value {after change} from pointer is:%d\n", *ptr);
